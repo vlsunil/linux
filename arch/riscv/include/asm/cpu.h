6c6bfabe4246a7d8c94a9d0ec3515636b32d9e8d
Sunil V L <sunilvl@ventanamicro.com>
2022-06-02 20:15:18 +0530

    RISC-V: ACPI: Add empty headers to enable ACPI core
    Few header files are required unconditionally by ACPI core.
    So add empty header files for now and update it when needed.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

---
+++ arch/riscv/include/asm/cpu.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2014 ARM Ltd.
+ * Copyright (C) 2023 Ventana Micro Systems Inc.
+ */
+#ifndef __ASM_CPU_H
+#define __ASM_CPU_H
+
+#endif /* __ASM_CPU_H */
