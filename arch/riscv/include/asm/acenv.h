9840a38f5f809900f63fa24f4fa13f4aa8cb574b
Sunil V L <sunilvl@ventanamicro.com>
2022-06-02 20:15:18 +0530

    RISC-V: ACPI: Add empty headers to enable ACPICA
    ACPICA requires few architecture specific header files.
    Add empty header filers so that build succeeds.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

---
+++ arch/riscv/include/asm/acenv.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * RISC-V specific ACPICA environments and implementation
+ *
+ * Copyright (C) 2014, Linaro Ltd.
+ *   Author: Hanjun Guo <hanjun.guo@linaro.org>
+ *   Author: Graeme Gregory <graeme.gregory@linaro.org>
+ * Copyright (C) 2021, Ventana Micro Systems Inc.
+ *   Author: Sunil V L <sunilvl@ventanamicro.com>
+ */
+
+#ifndef _ASM_ACENV_H
+#define _ASM_ACENV_H
+
+/* It is required unconditionally by ACPI core, update it when needed. */
+
+#endif /* _ASM_ACENV_H */
