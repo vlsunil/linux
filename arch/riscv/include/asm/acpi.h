f97e5bfb83958baaf7c667ce64035dd0cf5910b8
Sunil V L <sunilvl@ventanamicro.com>
2023-01-24 16:47:09 +0530

    ACPI: Enable ACPI_PROCESSOR for RISC-V
    The ACPI processor driver is not currently enabled for RISC-V.
    Enable it
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- arch/riscv/include/asm/acpi.h
+++ arch/riscv/include/asm/acpi.h
@@ -37,4 +37,21 @@ static inline void enable_acpi(void)
 	acpi_noirq = 0;
 }
 
+/*
+ * The ACPI processor driver for ACPI core code needs this macro
+ * to find out this cpu was already mapped (mapping from CPU hardware
+ * ID to CPU logical ID) or not.
+ */
+#define cpu_physical_id(cpu) cpuid_to_hartid_map(cpu)
+
+/*
+ * Since MADT must provide at least one RINTC structure, the
+ * CPU will be always available in MADT on RISC-V.
+ */
+static inline bool acpi_has_cpu_in_madt(void)
+{
+	return true;
+}
+
+static inline void arch_fix_phys_package_id(int num, u32 slot) { }
 #endif /*_ASM_ACPI_H*/
