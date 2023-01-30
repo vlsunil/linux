8af91ad7c031ae3e82a01a4b8a77f021401e3b28
Sunil V L <sunilvl@ventanamicro.com>
2023-01-30 15:45:33 +0530

    RISC-V: ACPI: smpboot: Add function to retrieve the hartid
    hartid is in RINTC structuire in MADT table. Instead of parsing
    the ACPI table every time we need for a cpu, cache it and provide
    a function to read it.
    
    This is similar to acpi_get_madt_gicc() in arm64.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

--- arch/riscv/include/asm/acpi.h
+++ arch/riscv/include/asm/acpi.h
@@ -65,6 +65,18 @@ int acpi_numa_get_nid(unsigned int cpu);
 static inline int acpi_numa_get_nid(unsigned int cpu) { return NUMA_NO_NODE; }
 #endif /* CONFIG_ACPI_NUMA */
 
-#endif
+struct acpi_madt_rintc *acpi_get_madt_rintc(int cpu);
+struct acpi_madt_rintc *acpi_cpu_get_madt_rintc(int cpu);
+static inline u32 get_acpi_id_for_cpu(int cpu)
+{
+	return	acpi_cpu_get_madt_rintc(cpu)->uid;
+}
+#else
+static inline u32 get_acpi_id_for_cpu(int cpu)
+{
+	return -1;
+}
+
+#endif /* CONFIG_ACPI */
 
 #endif /*_ASM_ACPI_H*/
