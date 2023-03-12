84568cd891c40626d9545ffd89adfebf4fbd1b12
Sunil V L <sunilvl@ventanamicro.com>
2023-03-12 22:25:23 +0530

    RISC-V: suspend.c: Move few functions from DT driver to generic
    Few functions like sbi_suspend() need to be called by both DT based
    cpuidle driver and ACPI LPI driver. So, move these common functions
    to a more generic suspend.c so that they can be shared.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

--- arch/riscv/kernel/suspend.c
+++ arch/riscv/kernel/suspend.c
@@ -90,6 +90,37 @@ int cpu_suspend(unsigned long arg,
 	return rc;
 }
 
+bool sbi_suspend_state_is_valid(u32 state)
+{
+	if (state > SBI_HSM_SUSPEND_RET_DEFAULT &&
+	    state < SBI_HSM_SUSPEND_RET_PLATFORM)
+		return false;
+	if (state > SBI_HSM_SUSPEND_NON_RET_DEFAULT &&
+	    state < SBI_HSM_SUSPEND_NON_RET_PLATFORM)
+		return false;
+	return true;
+}
+
+static int sbi_suspend_finisher(unsigned long suspend_type,
+				unsigned long resume_addr,
+				unsigned long opaque)
+{
+	struct sbiret ret;
+
+	ret = sbi_ecall(SBI_EXT_HSM, SBI_EXT_HSM_HART_SUSPEND,
+			suspend_type, resume_addr, opaque, 0, 0, 0);
+
+	return (ret.error) ? sbi_err_map_linux_errno(ret.error) : 0;
+}
+
+int sbi_suspend(u32 state)
+{
+	if (state & SBI_HSM_SUSP_NON_RET_BIT)
+		return cpu_suspend(state, sbi_suspend_finisher);
+	else
+		return sbi_suspend_finisher(state, 0, 0);
+}
+
 #ifdef CONFIG_RISCV_SBI
 static int sbi_system_suspend(unsigned long sleep_type,
 			      unsigned long resume_addr,
