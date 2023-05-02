a58208c26654a70f9812fff42b84a37d4e1aef55
Anup Patel <apatel@ventanamicro.com>
2023-05-02 12:57:34 +0530

    irqchip/riscv-imsic: Improve IOMMU DMA support
    We have a separate RISC-V IMSIC MSI address for each CPU so changing
    MSI (or IRQ) affinity results in re-programming of MSI address in
    the PCIe (or platform) device.
    
    Currently, the iommu_dma_prepare_msi() is called only once at the
    time of IRQ allocation so IOMMU DMA domain will only have mapping
    for one MSI page. This means iommu_dma_compose_msi_msg() called
    by imsic_irq_compose_msi_msg() will always use the same MSI page
    irrespective to target CPU MSI address. In other words, changing
    MSI (or IRQ) affinity for device using IOMMU DMA domain will not
    work.
    
    To address above issue, we do the following:
    1) Map MSI pages for all CPUs in imsic_irq_domain_alloc()
       using iommu_dma_prepare_msi().
    2) Add a new iommu_dma_select_msi() API to select a specific
       MSI page from a set of already mapped MSI pages.
    3) Use iommu_dma_select_msi() to select a specific MSI page
       before calling iommu_dma_compose_msi_msg() in
       imsic_irq_compose_msi_msg().
    
    Reported-by: Vincent Chen <vincent.chen@sifive.com>
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>

--- drivers/irqchip/irq-riscv-imsic.c
+++ drivers/irqchip/irq-riscv-imsic.c
@@ -446,6 +446,10 @@ static void imsic_irq_compose_msi_msg(struct irq_data *d,
 	if (WARN_ON(err))
 		return;
 
+	err = iommu_dma_select_msi(desc, msi_addr);
+	if (WARN_ON(err))
+		return;
+
 	msg->address_hi = upper_32_bits(msi_addr);
 	msg->address_lo = lower_32_bits(msi_addr);
 	msg->data = d->hwirq;
@@ -493,21 +497,24 @@ static int imsic_irq_domain_alloc(struct irq_domain *domain,
 	int i, hwirq, err = 0;
 	unsigned int cpu;
 
-	err = imsic_get_cpu(&imsic->lmask, false, &cpu);
-	if (err)
-		return err;
+	/* Map MSI address of all CPUs */
+	for_each_cpu(cpu, &imsic->lmask) {
+		err = imsic_cpu_page_phys(cpu, 0, &msi_addr);
+		if (err)
+			return err;
 
-	err = imsic_cpu_page_phys(cpu, 0, &msi_addr);
+		err = iommu_dma_prepare_msi(info->desc, msi_addr);
+		if (err)
+			return err;
+	}
+
+	err = imsic_get_cpu(&imsic->lmask, false, &cpu);
 	if (err)
 		return err;
 
 	hwirq = imsic_ids_alloc(get_count_order(nr_irqs));
 	if (hwirq < 0)
 		return hwirq;
-
-	err = iommu_dma_prepare_msi(info->desc, msi_addr);
-	if (err)
-		goto fail;
 
 	for (i = 0; i < nr_irqs; i++) {
 		imsic_id_set_target(hwirq + i, cpu);
@@ -528,10 +535,6 @@ static int imsic_irq_domain_alloc(struct irq_domain *domain,
 	}
 
 	return 0;
-
-fail:
-	imsic_ids_free(hwirq, get_count_order(nr_irqs));
-	return err;
 }
 
 static void imsic_irq_domain_free(struct irq_domain *domain,
