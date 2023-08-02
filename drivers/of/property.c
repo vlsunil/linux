bbf1501ecea2a3f5218f41b5fc6e3fb581cd6be0
Anup Patel <apatel@ventanamicro.com>
2023-08-02 18:00:25 +0530

    of: property: Add fw_devlink support for msi-parent
    This allows fw_devlink to create device links between consumers of
    a MSI and the supplier of the MSI.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>

--- drivers/of/property.c
+++ drivers/of/property.c
@@ -1267,6 +1267,7 @@ DEFINE_SIMPLE_PROP(resets, "resets", "#reset-cells")
 DEFINE_SIMPLE_PROP(leds, "leds", NULL)
 DEFINE_SIMPLE_PROP(backlight, "backlight", NULL)
 DEFINE_SIMPLE_PROP(panel, "panel", NULL)
+DEFINE_SIMPLE_PROP(msi_parent, "msi-parent", "#msi-cells")
 DEFINE_SUFFIX_PROP(regulators, "-supply", NULL)
 DEFINE_SUFFIX_PROP(gpio, "-gpio", "#gpio-cells")
 
@@ -1361,6 +1362,7 @@ static const struct supplier_bindings of_supplier_bindings[] = {
 	{ .parse_prop = parse_regulators, },
 	{ .parse_prop = parse_gpio, },
 	{ .parse_prop = parse_gpios, },
+	{ .parse_prop = parse_msi_parent, },
 	{}
 };
 
