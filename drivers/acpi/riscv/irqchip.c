4cdc4f19220704f177494a8e0620f559d1a2f93f
Sunil V L <sunilvl@ventanamicro.com>
2023-09-14 15:22:59 +0530

    ACPI: RISC-V: Create APLIC platform device
    Since APLIC needs to be a platform device, probe the MADT and create
    platform devices for each APLIC in the system.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

--- drivers/acpi/riscv/irqchip.c
+++ drivers/acpi/riscv/irqchip.c
@@ -5,6 +5,8 @@
  *
  */
 
+#include <linux/acpi.h>
+#include <linux/irqdomain.h>
 #include <linux/platform_device.h>
 #include <linux/irqchip/riscv-imsic.h>
 
@@ -24,8 +26,64 @@ void __init riscv_acpi_imsic_platform_init(void)
 	if (!pdev)
 		return;
 
-	pdev->dev.fwnode = imsic_acpi_get_fwnode();
+	pdev->dev.fwnode = imsic_acpi_get_fwnode(NULL);
 	ret = platform_device_add(pdev);
 	if (ret)
 		platform_device_put(pdev);
+}
+
+
+static int __init aplic_parse_madt(union acpi_subtable_headers *header,
+				   const unsigned long end)
+{
+	struct acpi_madt_aplic *aplic = (struct acpi_madt_aplic *)header;
+	struct platform_device *pdev;
+	struct fwnode_handle *fn;
+	struct resource *res;
+	int ret;
+
+	fn = irq_domain_alloc_named_id_fwnode("RISCV-APLIC", aplic->id);
+
+	pdev = platform_device_alloc("riscv-aplic", aplic->id);
+	if (!pdev)
+		return -ENOMEM;
+
+	res = kcalloc(1, sizeof(*res), GFP_KERNEL);
+	if (!res) {
+		platform_device_put(pdev);
+		return -ENOMEM;
+	}
+
+	res->start = aplic->base_addr;
+	res->end = res->start + aplic->size - 1;
+	res->flags = IORESOURCE_MEM;
+	ret = platform_device_add_resources(pdev, res, 1);
+	/*
+	 * Resources are duplicated in platform_device_add_resources,
+	 * free their allocated memory
+	 */
+	kfree(res);
+
+	/*
+	 * Add copy of aplic pointer so that platform driver get aplic details.
+	 */
+	ret = platform_device_add_data(pdev, &header, sizeof(header));
+	if (ret) {
+		platform_device_put(pdev);
+		return ret;
+	}
+
+	pdev->dev.fwnode = fn;
+	ret = platform_device_add(pdev);
+	if (ret) {
+		platform_device_put(pdev);
+		return ret;
+	}
+
+	return 0;
+}
+
+void __init riscv_acpi_aplic_platform_init(void)
+{
+	acpi_table_parse_madt(ACPI_MADT_TYPE_APLIC, aplic_parse_madt, 0);
 }
