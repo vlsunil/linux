d662f9fa54cbb48f91797405f2ae4682a3c0be0e
Sunil V L <sunilvl@ventanamicro.com>
2023-09-29 11:38:02 +0530

    irqchip: irq-sifive-plic: Add ACPI support
    Add ACPI support in PLIC driver. In ACPI, IO devices use Global System
    Interrupts (GSI) which is a flat space split across multiple PLICs. So,
    the driver also need to provide the mapping from GSI to correct PLIC.
    
    Signed-off-by: Haibo Xu <haibo1.xu@intel.com>
    Co-developed-by: Sunil V L <sunilvl@ventanamicro.com>
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

--- arch/riscv/include/asm/irq.h
+++ arch/riscv/include/asm/irq.h
@@ -12,6 +12,16 @@
 
 #include <asm-generic/irq.h>
 
+/*
+ * The ext_intc_id format is as follows:
+ * Bits [31:24] APLIC/PLIC ID
+ * Bits [15:0] APLIC IDC ID / PLIC S-Mode Context ID for this hart
+ */
+#define APLIC_PLIC_ID(x) ((x) >> 24)
+#define IDC_CONTEXT_ID(x) ((x) & 0x0000ffff)
+#define MAX_APLICS 32
+#define MAX_PLICS 32
+
 void riscv_set_intc_hwnode_fn(struct fwnode_handle *(*fn)(void));
 
 struct fwnode_handle *riscv_get_intc_hwnode(void);
